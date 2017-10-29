ccac12fcc73fe4179e297374ecf24b60faf16f57
Bui Do Hiep <buidohiep@gmail.com>
2017-10-29 17:05:10 +0800

    Update db path for shard and create remote command for run shards

--- config/run_config_remote.sh
+++ config/run_config_remote.sh
@@ -2,7 +2,7 @@
 for i in 50 51 52
 do
     echo "Run config server in node $i"
-    ssh xcnd$i "mongod --config ~/CS4224-MongoDB/config/config_server/xcnd$i.config"
+    ssh xcnd$i "/temp/mongodb/bin/mongod --config ~/CS4224-MongoDB/config/config_server/xcnd$i.config --fork --logpath /temp/mongodb_log/config_server.log"
 done
 
 wait
