4bb0f0398d5458c6f5cb24592d461708a49cd0b8
Bui Do Hiep <buidohiep@gmail.com>
2017-11-12 01:55:47 +0800

    Add import json script

--- load_data.sh
+++ load_data.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# Import all json file into db
+# Import all json file into db, require json file path as first argument
 
 echo "----------Import json file into MongoDB----------"
+for model in "warehouse" "district" "customer" "order" "item" "stock"
+do
+  mongoimport --host 192.168.51.13 --port 30020 --jsonArray --type json --file $1/$model.json --collection $model
+done
 
 
 echo "----------Done----------"
