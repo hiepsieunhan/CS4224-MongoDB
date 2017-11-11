27ea7670f9e69e904338fbf8da48cd7e1ac0b062
Bui Do Hiep <buidohiep@gmail.com>
2017-11-11 18:44:34 +0800

    Reimplement loading data with pipeline - missing merge order and order-lines

--- src/init/convertData.js
+++ src/init/convertData.js
@@ -1,6 +1,7 @@
-import fs from "fs";
+import fs, { write } from "fs";
 import readline from "readline";
-import stream from "stream";
+import { toASCII } from "punycode";
+import { resolve } from "path";
 
 const { DATA_DIR } = process.env;
 
@@ -13,18 +14,176 @@ if (!DATA_DIR) {
  * Utils methods
  */
 
-async function writeToJSONFile(data, filename) {
+const getFileNameFromDir = fileDir => {
+  const tokens = fileDir.split("/");
+  return tokens.length ? tokens[tokens.length - 1] : fileDir;
+};
+
+async function convertCSVtoJSON(inputFileDir, outputFileDir, modelConvert) {
   return new Promise((resolve, reject) => {
-    fs.writeFile(filename, JSON.stringify(data), function(err) {
-      if (err) {
-        console.error(`Can not save ${filename}! - error `, err);
-        reject(error);
-      } else {
-        resolve();
+    const outputStream = fs.createWriteStream(outputFileDir);
+    outputStream.write("[\n");
+    let isFirstElement = true;
+    const inputStream = fs.createReadStream(inputFileDir);
+    const rl = readline.createInterface({
+      input: inputStream,
+    });
+    rl.on("line", function(line) {
+      const model = modelConvert(line);
+      if (model) {
+        let convertedLine = JSON.stringify(model);
+        if (!isFirstElement) {
+          convertedLine = ",\n" + convertedLine;
+        } else {
+          isFirstElement = false;
+        }
+        outputStream.write(convertedLine);
       }
+    });
+
+    rl.on("close", function() {
+      console.log(
+        `Finish convert ${getFileNameFromDir(
+          inputFileDir,
+        )} to ${getFileNameFromDir(outputFileDir)}`,
+      );
+      outputStream.write("\n]");
+      outputStream.end();
+      resolve();
     });
   });
 }
+
+/**
+ *  Normal convert CSV to JSON methods
+ */
+
+async function convertWarehouses() {
+  const convertModel = row => {
+    const values = row.split(",");
+    if (values.length != 9) {
+      return null;
+    }
+    return {
+      w_id: parseInt(values[0]) || 0,
+      w_name: values[1],
+      w_street_1: values[2],
+      w_street_2: values[3],
+      w_city: values[4],
+      w_state: values[5],
+      w_zip: values[6],
+      w_tax: parseFloat(values[7]) || 0,
+      w_ytd: parseFloat(values[8]) || 0,
+    };
+  };
+
+  await convertCSVtoJSON(
+    `${DATA_DIR}/warehouse.csv`,
+    `${DATA_DIR}/warehouse.json`,
+    convertModel,
+  );
+}
+
+async function convertCustomers() {
+  const convertModel = row => {
+    const values = row.split(",");
+    if (values.length != 21) {
+      return null;
+    }
+    return {
+      c_w_id: parseInt(values[0]) || 0,
+      c_d_id: parseInt(values[1]) || 0,
+      c_id: parseInt(values[2]) || 0,
+      c_first: values[3],
+      c_middle: values[4],
+      c_last: values[5],
+      c_street_1: values[6],
+      c_street_2: values[7],
+      c_city: values[8],
+      c_state: values[9],
+      c_zip: values[10],
+      c_phone: values[11],
+      c_since: values[12],
+      c_credit: values[13],
+      c_credit_limit: parseFloat(values[14]) || 0,
+      c_discount: parseFloat(values[15]) || 0,
+      c_balance: parseFloat(values[16]) || 0,
+      c_ytd_payment: parseFloat(values[17]) || 0,
+      c_payment_cnt: parseInt(values[18]) || 0,
+      c_delivery_cnt: parseInt(values[19]) || 0,
+      c_data: values[20],
+    };
+  };
+  await convertCSVtoJSON(
+    `${DATA_DIR}/customer.csv`,
+    `${DATA_DIR}/customer.json`,
+    convertModel,
+  );
+}
+
+async function convertItems() {
+  const convertModel = row => {
+    const values = row.split(",");
+    if (values.length != 5) {
+      return null;
+    }
+    return {
+      i_id: parseInt(values[0]) || 0,
+      i_name: values[1],
+      i_price: parseFloat(values[2]) || 0,
+      i_im_id: parseInt(values[3]),
+      i_data: values[4],
+    };
+  };
+
+  await convertCSVtoJSON(
+    `${DATA_DIR}/item.csv`,
+    `${DATA_DIR}/item.json`,
+    convertModel,
+  );
+}
+
+// Note that this method read from tmp-stock.csv where the item name and item price are added
+async function convertStocks() {
+  const convertModel = row => {
+    const values = row.split(",");
+    if (values.length != 19) {
+      return null;
+    }
+    return {
+      s_w_id: parseInt(values[0]) || 0,
+      s_i_id: parseInt(values[1]) || 0,
+      s_quantity: parseFloat(values[2]) || 0,
+      s_ytd: parseFloat(values[3]) || 0,
+      s_order_cnt: parseInt(values[4]) || 0,
+      s_remote_cnt: parseInt(values[5]) || 0,
+      s_dist_01: values[6],
+      s_dist_02: values[7],
+      s_dist_03: values[8],
+      s_dist_04: values[9],
+      s_dist_05: values[10],
+      s_dist_06: values[11],
+      s_dist_07: values[12],
+      s_dist_08: values[13],
+      s_dist_09: values[14],
+      s_dist_10: values[15],
+      s_data: values[16],
+      s_i_name: values[17],
+      s_i_price: parseFloat(values[18]) || 0,
+    };
+  };
+  await convertCSVtoJSON(
+    `${DATA_DIR}/tmp-stock.csv`,
+    `${DATA_DIR}/stocks.json`,
+    convertModel,
+  );
+}
+
+/**
+ * Special methods:
+ *  - Fix Order by adding Order Lines and then write to JSON
+ *  - Fix District by countint number of delivered orders then write to JSOn
+ */
 
 async function readFile(filename) {
   return new Promise((resolve, reject) => {
@@ -40,157 +199,76 @@ async function readFile(filename) {
     });
 
     rl.on("close", function() {
-      console.log("Finish", data.length);
       resolve(data);
     });
   });
 }
 
-/**
- * Read csv and convert into JSON methods
- */
-
-// Note that this method does not write to file
-async function convertOrderLines() {
-  try {
-    const rows = await readFile(`${DATA_DIR}/tmp-order-line.csv`);
-    const orderLines = rows
-      .map(row => {
-        const values = row.split(",");
-        if (values.length != 11) {
-          return null;
-        }
-        return {
-          ol_w_id: parseInt(values[0]) || 0,
-          ol_d_id: parseInt(values[1]) || 0,
-          ol_o_id: parseInt(values[2]) || 0,
-          ol_number: parseInt(values[3]) || 0,
-          ol_i_id: parseInt(values[4]) || 0,
-          ol_delivery_d: values[5],
-          ol_amount: parseFloat(values[6]) || 0,
-          ol_supply_w_id: parseInt(values[7]) || 0,
-          ol_quantity: parseFloat(values[8]) || 0,
-          ol_dist_info: values[9],
-          ol_i_name: values[10],
-        };
-      })
-      .filename(row => row);
-    return orderLines;
-  } catch (err) {
-    process.exit(1);
-  }
-}
-
-// Put in all order lines into its corresponding orders.
-async function fixOrders(orders) {
-  const orderLines = await convertOrderLines();
-  const orderMap = new Map();
-  // put all orders into map
-  orders.forEach(order => {
-    const { o_w_id, o_d_id, o_id } = order;
-    const key = `${o_w_id}_${o_d_id}_${o_id}`;
-    orderMap.set(key, order);
+async function writeToJSONFile(data, filename) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, JSON.stringify(data), function(err) {
+      if (err) {
+        console.error(`Can not save ${filename}! - error `, err);
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
   });
-  orderLines.forEach(orderLine => {
-    const { ol_w_id, ol_d_id, ol_o_id, ol_delivery_d, ...rest } = orderLine;
-    const key = `${ol_w_id}_${ol_d_id}_${ol_o_id}`;
-    const order = orderMap.get(key);
-    if (!order) {
-      return;
-    }
-    // If this is the first order line of this order
-    if (!order.o_order_lines) {
-      order.o_order_lines = [];
-      order.o_delivery_d = ol_delivery_d;
-    }
-    order.o_order_lines.push({ ...rest });
-  });
-  return orders;
 }
 
-async function convertOrders() {
-  try {
-    const rows = await readFile(`${DATA_DIR}/order.csv`);
-    const orders = rows
-      .map(row => {
-        const values = row.split(",");
-        if (values.length != 8) {
-          return null;
-        }
-        return {
-          o_w_id: parseInt(values[0]) || 0,
-          o_d_id: parseInt(values[1]) || 0,
-          o_id: parseInt(values[2]) || 0,
-          o_c_id: parseInt(values[3]) || 0,
-          o_carrier_id: parseInt(values[4]) || 0,
-          o_ol_cnt: parseInt(values[5]) || 0,
-          o_all_local: parseInt(values[6]) || 0,
-          o_entry_d: values[7],
-          o_delivery_d: "",
-        };
-      })
-      .filename(row => row);
-
-    const fixedOrders = await fixOrders(orders);
-    await writeToJSONFile(fixedOrders, `${DATA_DIR}/orders.json`);
-    return fixedOrders;
-  } catch (err) {
-    process.exit(1);
+const orderModelConvert = row => {
+  const values = row.split(",");
+  if (values.length != 8) {
+    return null;
   }
-}
+  return {
+    o_w_id: parseInt(values[0]) || 0,
+    o_d_id: parseInt(values[1]) || 0,
+    o_id: parseInt(values[2]) || 0,
+    o_c_id: parseInt(values[3]) || 0,
+    o_carrier_id: parseInt(values[4]) || 0,
+    o_ol_cnt: parseInt(values[5]) || 0,
+    o_all_local: parseInt(values[6]) || 0,
+    o_entry_d: values[7],
+    o_delivery_d: "",
+  };
+};
 
 // Fix the value of d_next_deliver_o_id
 async function fixDistricts(districts) {
-  const orders = await convertOrders();
-  const districtMap = new Map();
-  districts.forEach(district => {
-    const key = `${district.d_w_id}_${district.d_id}`;
-    districtMap.set(key, district);
-  });
-  orders.forEach(order => {
-    const key = `${order.o_w_id}_${order.o_d_id}`;
-    if (order.o_carrier_id) {
-      const district = districtMap.get(key);
-      if (district) {
-        district.d_next_deliver_o_id += 1;
-      }
-    }
-  });
-  return districts;
-}
+  return new Promise((resolve, reject) => {
+    const districtMap = new Map();
+    districts.forEach(district => {
+      const key = `${district.d_w_id}_${district.d_id}`;
+      districtMap.set(key, district);
+    });
 
-async function convertWarehouses() {
-  try {
-    const rows = await readFile(`${DATA_DIR}/warehouse.csv`);
-    const warehouses = rows
-      .map(row => {
-        const values = row.split(",");
-        if (values.length != 9) {
-          return null;
+    const inputStream = fs.createReadStream(`${DATA_DIR}/tmp-order.csv`);
+    const rl = readline.createInterface({
+      input: inputStream,
+    });
+    rl.on("line", function(line) {
+      const order = orderModelConvert(line);
+      if (order) {
+        const key = `${order.o_w_id}_${order.o_d_id}`;
+        if (order.o_carrier_id) {
+          const district = districtMap.get(key);
+          if (district) {
+            district.d_next_deliver_o_id += 1;
+          }
         }
-        return {
-          w_id: parseInt(values[0]) || 0,
-          w_name: values[1],
-          w_street_1: values[2],
-          w_street_2: values[3],
-          w_city: values[4],
-          w_state: values[5],
-          w_zip: values[6],
-          w_tax: parseFloat(values[7]) || 0,
-          w_ytd: parseFloat(values[8]) || 0,
-        };
-      })
-      .filter(row => row);
-
-    await writeToJSONFile(warehouses, `${DATA_DIR}/warehouse.json`);
-    return warehouses;
-  } catch (err) {
-    process.exit(1);
-  }
+      }
+    });
+    rl.on("close", function() {
+      resolve(districts);
+    });
+  });
 }
 
 async function convertDistricts() {
   try {
+    // Note: We can read the whole file district as the number of district is small
     const rows = await readFile(`${DATA_DIR}/district.csv`);
     const districts = rows
       .map(row => {
@@ -216,126 +294,25 @@ async function convertDistricts() {
       })
       .filter(row => row);
 
-    const fixedDistricts = fixDistricts(districts);
+    const fixedDistricts = await fixDistricts(districts);
 
     await writeToJSONFile(fixedDistricts, `${DATA_DIR}/district.json`);
-    return fixedDistricts;
+    console.log(
+      "Finish convert district.csv to district.json by fixing value next_deliver_o_id",
+    );
   } catch (err) {
     process.exit(1);
   }
 }
 
-async function convertCustomers() {
-  try {
-    const rows = await readFile(`${DATA_DIR}/customer.csv`);
-    const customers = rows
-      .map(row => {
-        const values = row.split(",");
-        if (values.length != 21) {
-          return null;
-        }
-        return {
-          c_w_id: parseInt(values[0]) || 0,
-          c_d_id: parseInt(values[1]) || 0,
-          c_id: parseInt(values[2]) || 0,
-          c_first: values[3],
-          c_middle: values[4],
-          c_last: values[5],
-          c_street_1: values[6],
-          c_street_2: values[7],
-          c_city: values[8],
-          c_state: values[9],
-          c_zip: values[10],
-          c_phone: values[11],
-          c_since: values[12],
-          c_credit: values[13],
-          c_credit_limit: parseFloat(values[14]) || 0,
-          c_discount: parseFloat(values[15]) || 0,
-          c_balance: parseFloat(values[16]) || 0,
-          c_ytd_payment: parseFloat(values[17]) || 0,
-          c_payment_cnt: parseInt(values[18]) || 0,
-          c_delivery_cnt: parseInt(values[19]) || 0,
-          c_data: values[20],
-        };
-      })
-      .filter(row => row);
-
-    await writeToJSONFile(customers, `${DATA_DIR}/customer.json`);
-    return customers;
-  } catch (err) {
-    process.exit(1);
-  }
-}
-
-async function convertItems() {
-  try {
-    const rows = await readFile(`${DATA_DIR}/item.csv`);
-    const items = rows
-      .map(row => {
-        const values = row.split(",");
-        if (values.length != 5) {
-          return null;
-        }
-        return {
-          i_id: parseInt(values[0]) || 0,
-          i_name: values[1],
-          i_price: parseFloat(values[2]) || 0,
-          i_im_id: parseInt(values[3]),
-          i_data: values[4],
-        };
-      })
-      .filter(row => row);
-
-    await writeToJSONFile(items, `${DATA_DIR}/item.json`);
-    return items;
-  } catch (err) {
-    process.exit(1);
-  }
-}
-
-// Note that this method read from tmp_stock.csv where the item name and item price are added
-async function convertStocks() {
-  try {
-    const rows = await readFile(`${DATA_DIR}/tmp-stock.csv`);
-    const stocks = rows
-      .map(row => {
-        const values = row.split(",");
-        if (values.length != 19) {
-          return null;
-        }
-        return {
-          s_w_id: parseInt(values[0]) || 0,
-          s_i_id: parseInt(values[1]) || 0,
-          s_quantity: parseFloat(values[2]) || 0,
-          s_ytd: parseFloat(values[3]) || 0,
-          s_order_cnt: parseInt(values[4]) || 0,
-          s_remote_cnt: parseInt(values[5]) || 0,
-          s_dist_01: values[6],
-          s_dist_02: values[7],
-          s_dist_03: values[8],
-          s_dist_04: values[9],
-          s_dist_05: values[10],
-          s_dist_06: values[11],
-          s_dist_07: values[12],
-          s_dist_08: values[13],
-          s_dist_09: values[14],
-          s_dist_10: values[15],
-          s_data: values[16],
-          s_i_name: values[17],
-          s_i_price: parseFloat(values[18]) || 0,
-        };
-      })
-      .filter(row => row);
-
-    await writeToJSONFile(stocks, `${DATA_DIR}/stocks.json`);
-    return stocks;
-  } catch (err) {
-    process.exit(1);
-  }
+// Note: This method is very complicated because we need to read 2 large file parallel...
+async function convertOrders() {
+  // const orderlineStream = fs.createReadStream(`${DATA_DIR}/tmp-order-line.csv`);
 }
 
 convertWarehouses();
-convertCustomers();
 convertDistricts();
+convertCustomers();
+convertOrders();
 convertItems();
 convertStocks();
